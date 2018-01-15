9f7a500407e6238fb5d00eb3fa1af544c1b80455
ShlokD <shlokpdesai@gmail.com>
2018-01-14 16:06:59 -0800

    Clap button

---
+++ README.md
@@ -0,0 +1,3 @@
+### Component Storybook
+
+Some demos of React components built using Create-React-App and React Storybook
---
+++ src/components/buttons.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import './clap-button.css';
+
+export class ClapButton extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clapCount: 0,
+      clapCounterVisible: false
+    };
+
+    this._onClick = this._onClick.bind(this);
+    this._resetClapCounter =  this._resetClapCounter.bind(this);
+  }
+
+  _onClick() {
+    if (this.state.clapCount < this.props.maxClap) {
+      this.setState({
+        clapCount: this.state.clapCount + 1,
+        clapCounterVisible: true
+      });
+      setTimeout(this._resetClapCounter, 800);
+    }
+  }
+
+  _resetClapCounter() {
+    this.setState({
+      clapCounterVisible: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="clap-button-container">
+        <div className={`clap-counter ${this.state.clapCounterVisible ? 'is-visible': 'is-hidden'}`}>{this.state.clapCount}</div>
+        <button className="clap-button" onClick={this._onClick}></button>
+      </div>
+    );
+  }
+};
+
+ClapButton.defaultProps = {
+  maxClap: 10
+};
